dcb14e166159fab3d9789fb24bb99e69ae40a090
Brett Barnett <brett_barnett@sbcglobal.net>
2024-03-18 21:53:39 -0500

    Completed Graded Assignment #6 Orbit Report

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,29 @@
+import './styling.css';
+
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type Of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sat.map((satellite) => {
+          return (
+            <tr key={satellite.id}>
+              <td>{satellite.name}</td>
+              <td>{satellite.type}</td>
+              <td>{satellite.launchDate}</td>
+              <td>{satellite.operational ? "Active" : "Inactive"}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
